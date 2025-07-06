915b406ea3ce181e64c31e582de3c81f43919eda
rMiccolis <r.miccolis1@gmail.com>
2025-07-06 13:02:29 +0200

    substitution for variables inside mongodb_values.yaml

--- bin/install_mongodb.sh
+++ bin/install_mongodb.sh
@@ -2,6 +2,36 @@
 
 # Install mongodb
 
+# function needs a file name parameter to operate the substitution on ${var} and $var variables type
+# REQUIRED ARGUMENT: file_name
+envsubst_preserve_empty_variables(){
+file_name=$1
+# Step 1: Extracts all variables in the format ${VAR} or $VAR from the file
+vars=$(grep -oP '\$\{([a-zA-Z_][a-zA-Z0-9_]*)\}|\$([a-zA-Z_][a-zA-Z0-9_]*)' $file_name | sort -u)
+temp_vars=($vars)
+if [[ ${#temp_vars[@]} -gt 0 ]]; then
+  # echo "variables to be substituted: $vars"
+
+  # Step 2: For each variable found, check whether it is defined in the environment
+  while read -r var; do
+    # Extracts the variable name, without the parentheses `${}`
+    var_name=$(echo "$var" | sed -E 's/\$\{([^}]+)\}/\1/')
+
+    # Extracts the variable name, without the dollar `$`
+    var_name=$(echo "$var_name" | sed -E 's/\$//g')
+
+    # If the variable is defined in the environment, replace it
+    if [[ -n "${!var_name}" ]]; then
+      # Replaces ${var} with the value of the environment variable
+      sed -i "s|\${$var_name}|${!var_name}|g" $file_name
+      # Replaces $var with the value of the environment variable
+      sed -i "s|\$${var_name}|${!var_name}|g" $file_name
+    fi
+  done <<< "$vars"
+fi
+# Now the $file_name contains only the replaced variables that are defined in the environment
+}
+
 # setting variables for tls certificate
 KEY_FILE="tls-cert.key"
 CERT_FILE="mongodb.pem"
@@ -37,6 +67,8 @@ else
 fi
 
 # use the provided /home/$USER/mongodb_values.yaml to configure the bitnami mongodb helm chart
+# and substitute any environment variables
+envsubst_preserve_empty_variables /home/$USER/mongodb_values.yaml
 helm install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb -n mongodb -f /home/$USER/mongodb_values.yaml
 
 kubectl rollout status deployment mongodb -n mongodb --timeout=3000s > /dev/null 2>&1
